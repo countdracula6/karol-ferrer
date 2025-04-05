ec29aa7f0275694e506efba353da56e5d78ec501
Paulo Craice <paulocraice@gmail.com>
2025-04-05 07:14:52 -0300

    Atualizar o scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -18,3 +18,30 @@ function toggleCommentBox() {
   commentBox.style.display =
     commentBox.style.display === "block" ? "none" : "block";
 }
+
+// HOME already works by default with <a href="#home">
+
+// Bookmark Karol
+function bookmarkKarol() {
+  alert("Karol Ferrer has been bookmarked in your heart!");
+}
+
+// Share Karol Profile
+function shareKarol() {
+  if (navigator.share) {
+    navigator.share({
+      title: 'Karol Ferrer VIP',
+      text: 'Check out Karol Ferrer VIP site!',
+      url: window.location.href,
+    })
+    .then(() => console.log('Thanks for sharing!'))
+    .catch((error) => console.log('Share failed:', error));
+  } else {
+    alert('Sharing is not supported on your device.');
+  }
+}
+
+// Care Message
+function showCareMessage() {
+  alert("I'll be back soon, baby. Waiting for you...");
+}

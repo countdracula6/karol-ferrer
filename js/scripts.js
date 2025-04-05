b4cedc0b05e68a01483c36776c1ccc0ace0b505a
Paulo Craice <paulocraice@gmail.com>
2025-04-05 09:26:37 -0300

    Improve Modals

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,13 @@
 function openIframeModal(url) {
-  document.getElementById("iframeModal").style.display = "block";
-  document.getElementById("iframeModalSrc").src = url;
-}
-
-function openPaymentModal() {
-  document.getElementById("paymentModal").style.display = "block";
+  const modal = document.getElementById("iframeModal");
+  const iframe = document.getElementById("iframeModalSrc");
+  iframe.src = url;
+  modal.style.display = "block";
 }
 
 function closeModal() {
   document.getElementById("iframeModal").style.display = "none";
-  document.getElementById("paymentModal").style.display = "none";
-  document.getElementById("iframeModalSrc").src = ""; // Clear URL
+  document.getElementById("iframeModalSrc").src = ""; // Clean iframe
 }
 
 function toggleCommentBox() {
